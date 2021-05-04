6c9b4e36fff8c7a659fdc5795e83b508c9b020a4
shambu <shambuqwerty@gmail.com>
2021-05-05 01:35:46 +0530

    deploy ready

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+require('dotenv').config();
+const axios = require("axios");
+
+const app = express();
+const token = process.env.TOKEN;
+const port = process.env.PORT
+const tel = `https://api.telegram.org/bot${token}/sendMessage`;
+
+
+const awareness = "FUCK COVID.";
+const symptoms  = "aches and pains, sore throat, diarrhoea, mental retardness";
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+
+app.get("/", (req, res)=>{
+    res.send("Welcome to Covid bot v2.0\n Commands:1) \\aware\n 2)\\simptons");
+});
+
+
+app.post("/", (req, res)=>{
+    const { message } = req.body;
+    reply = "Welcome to Covid bot v2.0\n Commands:1) \\aware\n 2)\\simptons";
+    if(message.text.toLowerCase().indexOf("/start") !== -1)
+        sendMessage(tel,message,reply,res);
+    else if(message.text.toLowerCase().indexOf("/aware") !== -1)
+        sendMessage(tel,message,awareness,res);
+    else if(message.text.toLowerCase().indexOf("/simptons") !== -1)
+        sendMessage(tel,message,symptoms,res);
+    else
+        sendMessage(tel,message,"Invalid Command",res);
+});
+
+app.listen(port, () => console.log(`Telegram bot is listening on port ${port}!`));
